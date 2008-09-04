15a4a62c8bbd4e877d5ce63d6074d3b3b779cc66
dfdeshom@gmail.com <>
2008-09-04 08:49:59 -0400

    more little additions

--- v8onpython.pyx
+++ v8onpython.pyx
@@ -1,17 +1,36 @@
 # didier deshommes
 cdef extern from "include/v8.h":
-    ctypedef struct c_value "v8::Number":
-        void (*Int32Value)()
-    
-    c_value cvalue_factory "v8::Number::New"(int i)
+    ctypedef struct c_handlescope "v8::HandleScope":
+        pass
+    void** c_create_handle "v8::HandleScope::CreateHandle"(void*)
 
+    # Context
+    ctypedef struct c_context "v8::Handle<v8::Context>":
+        pass
+    c_context c_context_factory "v8::Context::New"()
+
+    # Value
+    ctypedef struct c_value "v8::Handle<v8::Value>":
+        pass
     
 cdef class Value:
-    cdef c_value c 
-    def __init__(self):
-        """
-        test
-        """
-        print 0  
-        #cvalue_factory(2)
- 
+    cdef c_handlescope handlescope
+    cdef c_value c
+    cdef c_context context
+
+    def __cinit__(self):
+        """  
+        Create a Context() 
+        """ 
+        cdef int p = 0 
+        cdef c_handlescope h          
+        c_create_handle(<void*>p)
+
+    def set_value(self,int val):
+        pass
+        #cdef c_value r = <c_value>cstring_factory('2',1)
+        
+    def print_value(self):
+        pass
+        #r= (<c_value>self.c).Int32Value()
+        #return r
