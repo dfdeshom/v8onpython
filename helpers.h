82452bd3921544e444e85e597e5bd46c876a074a
dfdeshom@gmail.com <>
2008-09-03 20:13:16 -0400

    v8 sources

--- helpers.h
+++
@@ -1,6 +0,0 @@
-// didier deshommes <dfdeshom@gmail.com>
-// BSD license
-#include "v8-src/include/v8.h"
-#define HANDLESCOPE() v8::HandleScope scope
-#define ATTACH_SCOPE_TO_CONTEXT(context)  v8::Context::Scope s = v8::Context::Scope (context)
-#define RUN_SCRIPT(script) script->Run()
