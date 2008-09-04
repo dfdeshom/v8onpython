6fefb204d15c1529a4a69408c3e91afe830d60e0
dfdeshom@gmail.com <>
2008-09-04 04:32:03 -0400

    checking in

--- v8onpython.pyx
+++ v8onpython.pyx
@@ -1,10 +1,17 @@
 # didier deshommes
-#cdef extern from "include/v8.h":
-#    ctypedef struct c_value "v8::Handle<v8::String>":
-#        pass
-#    c_value new_value "v8::String::New"(char *e,int length)
-  
-def do():
-    return 0
+cdef extern from "include/v8.h":
+    ctypedef struct c_value "v8::Number":
+        void (*Int32Value)()
+    
+    c_value cvalue_factory "v8::Number::New"(int i)
 
+    
+cdef class Value:
+    cdef c_value c 
+    def __init__(self):
+        """
+        test
+        """
+        print 0  
+        #cvalue_factory(2)
  
