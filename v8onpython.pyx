866689372784ba14f39822646cb40bf073e36ab2
dfdeshom@aida <>
2008-09-04 13:56:48 -0400

    add a prototype of a function to compile code

--- v8onpython.pyx
+++ v8onpython.pyx
@@ -3,31 +3,60 @@
 cdef extern from "scopehandle.h":
     void HANDLESCOPE() 
 
-cdef extern from "include/v8.h":
-    ctypedef struct c_handlescope "v8::HandleScope":
-        pass
-    void** c_create_handle "v8::HandleScope::CreateHandle"(void*)
-
     # Context
     ctypedef struct c_context "v8::Handle<v8::Context>":
         pass
     c_context c_context_factory "v8::Context::New"()
 
+    # Scope
+    ctypedef struct c_scope "v8::Context::Scope":
+        pass
+    c_scope c_scope_factory "v8::Context::Scope _scope"(c_context)
+
     # Value
     ctypedef struct c_value "v8::Handle<v8::Value>":
         pass
-    
-cdef class Value:
-    cdef c_handlescope handlescope
-    cdef c_value c
-    cdef c_context context
 
+    # String
+    ctypedef struct c_string "v8::Handle<v8::String>":
+        void (* Length)()
+    c_string c_string_factory "v8::String::New"(char *s)
+    
+    # Script
+    ctypedef struct c_script "v8::Handle<v8::Script>":
+        pass
+    c_script c_script_fatory "v8::Script::Compile"(c_string)
+
+
+cdef class Value:
+    #cdef c_context context
+    
     def __cinit__(self):
         """  
         Create a Context() 
         """
+        # Create a handle scope
         HANDLESCOPE()
         
+        # Create a new context
+        c_context_factory()
+
+        # New scope
+        
+    def compile(self,code):
+        # handle scope
+        HANDLESCOPE()
+        print 'done 1'
+        # Create a new context
+        cdef c_context context = c_context_factory()
+        print 'done 2'
+        # Context scope
+        cdef c_scope scope = c_scope_factory(context)
+        print 'done 3'
+        cdef c_string clone = c_string_factory(code)
+        print 'done 4'
+        cdef c_script script = c_script_fatory(clone)
+
     def set_value(self,int val):
         pass
         #cdef c_value r = <c_value>cstring_factory('2',1)
