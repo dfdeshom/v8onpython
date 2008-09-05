14fbb538c14ce4718329130e7db6f91813e7f43f
dfdeshom@aida <>
2008-09-05 11:57:45 -0400

    finally able to compile any script to JS

--- scopehandle.h
+++ scopehandle.h
@@ -3,13 +3,3 @@
 #define HANDLESCOPE() v8::HandleScope scope
 #define ATTACH_SCOPE_TO_CONTEXT(context)  v8::Context::Scope s = v8::Context::Scope (context)
 #define RUN_SCRIPT(script) script->Run()
-
-#include <stdio.h>
-#include <string.h>
-
-inline void ascii(v8::Handle<v8::Value> handle,char *res)
-{
-  v8::String::AsciiValue ascii(handle);
-  memcpy(res,*ascii,strlen(*ascii)+1);
-  
-}
