7a21d597b34ddf60408fec4ce8bdce18f722ac56
dfdeshom@aida <>
2008-09-05 09:42:46 -0400

    compile, run script

--- scopehandle.h
+++ scopehandle.h
@@ -1,3 +1,15 @@
 // didier deshommes
 #include "include/v8.h"
 #define HANDLESCOPE() v8::HandleScope scope
+#define ATTACH_SCOPE_TO_CONTEXT(context)  v8::Context::Scope s = v8::Context::Scope (context)
+#define RUN_SCRIPT(script) script->Run()
+
+#include <stdio.h>
+#include <string.h>
+
+inline void ascii(v8::Handle<v8::Value> handle,char *res)
+{
+  v8::String::AsciiValue ascii(handle);
+  memcpy(res,*ascii,strlen(*ascii)+1);
+  
+}
